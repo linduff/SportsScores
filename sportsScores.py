e1e880d9ba6b831602358441f1817107257e0981
linduff <calinduff@gmail.com>
2023-04-24 10:12:39 -0400

    added get json

--- sportsScores.py
+++ sportsScores.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
+from urllib.request import urlopen
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def scores():
-    return render_template('sportsScores.html')
+    url = 'http://site.api.espn.com/apis/site/v2/sports/baseball/mlb/scoreboard'
+    response = urlopen(url)
+    data_json = json.loads(response.read())
+    return render_template('sportsScores.html', data=data_json)
